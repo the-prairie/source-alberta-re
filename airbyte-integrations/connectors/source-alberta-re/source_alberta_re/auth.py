c223abda98df178f1323e75aa810e6cf5cc8642e
Lauren Zary <lzary@atb.com>
2022-07-09 14:51:59 -0700

    Fix issue with fake_useragent error

--- airbyte-integrations/connectors/source-alberta-re/source_alberta_re/auth.py
+++ airbyte-integrations/connectors/source-alberta-re/source_alberta_re/auth.py
@@ -23,11 +23,20 @@ class EmailAuthenticator(Oauth2Authenticator):
         self._token_expiry_date = token_expiry_date or pendulum.now().subtract(days=1)
         self._access_token = None
         
+    def get_user_agent(self) -> str:
+        try:
+            user_agent = UserAgent(verify_ssl=False).random
+        # Workaround for error occurred during loading data. 
+        # Trying to use cache server https://fake-useragent.herokuapp.com/browsers/0.1.11
+        except:
+            user_agent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36"
+        return user_agent
+    
     def get_headers(self) -> Mapping[str, Any]:
         
         headers: MutableMapping[str, Any] = {
             "Accept": "application/json",
-            "User-Agent": UserAgent(verify_ssl=False).random
+            "User-Agent": self.get_user_agent()
         }
         return headers
     
